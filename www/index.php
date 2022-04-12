aa3b298c3bca915429ae7429de54553e49a0f853
01jackob01 <jakub.mickiewicz96@wp.pl>
2022-04-12 19:44:55 +0200

    zmiany

--- www/index.php
+++ www/index.php
@@ -1,7 +1,18 @@
-<a href="index.php">Test1</a>
 <?php
-$a = 'aaa';
-echo 'Test111';
+$servername = "db";
+$username = "root";
+$password = "haslohaslo123";
+$db = 'test';
+$port = 3306;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db, $port);
+var_dump($conn);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 
 
